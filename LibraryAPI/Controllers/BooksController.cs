06bfb7945f47d53223cd2813c30e35295fccd6af
unknown <kamilawad15@gmail.com>
2024-05-24 20:58:15 +0300

    add get all books

---
+++ LibraryAPI/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using LibraryAPI.Data;
+using LibraryAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly LibraryContext _context;
+
+        public BooksController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetAllBooks()
+        {
+            var books = await _context.Books.ToListAsync();
+
+            return Ok(books);
+        }
+    }
+}
