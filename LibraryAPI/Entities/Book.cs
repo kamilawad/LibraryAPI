de0c5e0d870847e8c3bd34d7af7f800ae3837b42
unknown <kamilawad15@gmail.com>
2024-05-24 19:43:39 +0300

    create book entity

---
+++ LibraryAPI/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryAPI.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
